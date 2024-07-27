d2a75d49859048d419dad463d0e5bef016d282ce
emirkacar <emirkacar86@gmail.com>
2024-07-27 16:04:21 +0300

    Add files via upload

--- SoruCevapUygulamasi.py
+++ SoruCevapUygulamasi.py
@@ -1,43 +1,90 @@
+
+import random
+
 class Soru:
-    def __init__(self, metin, secimler, cevap):
-        self.metin = metin
-        self.secimler = secimler
-        self.cevap = cevap
-        self.skor = 0
-        self.index = 0
+    def __init__(self,soruMetni,secenekler,dogruCevap):
+        self.soruMetni=soruMetni
+        self.secenekler=secenekler
+        self.dogruCevap=dogruCevap
 
-    def soruyuAl(self):
-        print(self.metin)
-        
-
-    def secenekleriGoster(self):
-        for secenek in self.secimler:
-            print("-", secenek)
-
-    def tahminEt(self):
-        tahmin = input("Cevap: ")
-        if tahmin == self.cevap:
-            print("Doğru bildiniz, tebrikler.")
-            self.skor += 1
+    def soruSor(self):
+        print(f"{self.soruMetni}") 
+        for i,secenek in enumerate(self.secenekler):
+            print(f"{i}. {secenek}")
+    
+    def cevapKontrol(self,cevap):
+        if(cevap==self.dogruCevap):
+            print("Dogru cevap verdiniz,tebrikler.")
+            return True
         else:
-            print("Yanlis cevap.")
-        self.index += 1
+            print("Yanlis cevap verdiniz.")  
+            return False      
 
-    def skoruGoster(self):
-        print("Skorunuz =", self.skor)
+class Quiz:
+    def __init__(self):
+        self.sorular=[]
 
-    def oyunBittiMi(self,toplamSoruSayisi):
+    def soruEkle(self,soru):
+        self.sorular.append(soru)
+    
+    def quizBaslat(self):
+        random.shuffle(self.sorular)
+        print("Quiz baslamistir.")
+        return self.sorular
+    
+    def sonuclariGoster(self,puan,toplam):
+        print(f"\nQuiz bitti! Skorunuz: {puan}/{toplam}")
+        basariYuzdesi = (puan / toplam) * 100
+        print(f"Basari yuzdesi: {basariYuzdesi}%") 
         
-        if self.index >= toplamSoruSayisi:
-            print("Oyun bitmiştir.")
+
+class Ogrenci:
+    def __init__(self,ad):
+        self.ad=ad 
+        self.puan=0
+    
+    def cevapVer(self):
+        cevap=input("Cevap veriniz(1-4): ")
+        return int(cevap)
+    
+    def puanEkle(self):
+        self.puan += 1
+
+class QuizUygulamasi:
+    def __init__(self):
+        self.quiz=Quiz()
+        self.oyuncu=None
+        
+        
+    
+    def quizOlustur(self):
+        q1 = Soru("Türkiye'nin baskenti neresidir?", ["İstanbul", "Ankara", "İzmir", "Bursa"], 2)
+        q2 = Soru("5+5=?", ["8", "9", "7", "10"], 4)
+        q3 = Soru("Bir yilda kaç ay vardir?", ["10", "11", "12", "13"], 3)
+
+        self.quiz.soruEkle(q1)
+        self.quiz.soruEkle(q2)
+        self.quiz.soruEkle(q3)
+
+    def oyunuBaslat(self):
+        print("OYUNA HOSGELDINIZ")
+        kullaniciIsmi=input("Kullanici adiniz: ")
+        self.oyuncu =Ogrenci(kullaniciIsmi)
+
+        sorular=self.quiz.quizBaslat()
+        for soru in sorular:
+            soru.soruSor()
+            kullanicininCevabi=int(self.oyuncu.cevapVer())
+            if soru.cevapKontrol(kullanicininCevabi):  # cevapKontrol metodunu kullanın
+                self.oyuncu.puanEkle()
+           
+        
+        self.quiz.sonuclariGoster(self.oyuncu.puan,len(sorular))
+            
 
 
-# Soru nesnesi oluşturma
-s1 = Soru("En iyi programlama dili hangisi ?", ["C#", "Python", "Javascript", "Java"], "Python")
+uygulama=QuizUygulamasi()
+uygulama.quizOlustur()
+uygulama.oyunuBaslat()
 
-# Soruyu gosterme ve secenekleri gosterme
-s1.soruyuAl()
-s1.secenekleriGoster()  # Bu satirda seçeneklerin dogru sekilde gorunmesi saglaniyor.
-s1.tahminEt()
-s1.skoruGoster()
-s1.oyunBittiMi(1)
+            
